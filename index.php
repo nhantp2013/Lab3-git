285d882d33b32ac8d94f2e8840fca5e713fc3854
nhantp2013 <nhantp2013@gmail.com>
2021-03-31 16:18:46 +0700

    delete alot

--- index.php
+++ index.php
@@ -2,10 +2,5 @@
     <ul class="menu">
         <li>
             <a href="/LAB3/list_product.php">DANH SÁCH SẢN PHẨM< aaaaaaa/a>
-        </li>
-        <li>
-            <a href="/LAB3/add_product.php">THÊM SẢN PHẨM bbbbbbb</a>
-        </li>
-    
-    </ul>
+
 <?php include_once("footer.php"); ?>
--- index123.php
+++ index123.php
@@ -3,9 +3,6 @@
         <li>
             <a href="/LAB3/list_product.php">DANH SÁCH SẢN PHẨM</a>
         </li>
-        <li>
-            <a href="/LAB3/add_product.php">THÊM SẢN PHẨM</a>
-        </li>
-    
+
     </ul>
 <?php include_once("footer.php"); ?>
