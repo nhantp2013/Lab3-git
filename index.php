f835020b13e10f734051136048b40dc44d6344e6
nhantp2013 <nhantp2013@gmail.com>
2021-03-31 14:17:08 +0700

    message after reset

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include_once("header.php"); ?>
+    <ul class="menu">
+        <li>
+            <a href="/LAB3/list_product.php">DANH SÁCH SẢN PHẨM</a>
+        </li>
+        <li>
+            <a href="/LAB3/add_product.php">THÊM SẢN PHẨM</a>
+        </li>
+    
+    </ul>
+<?php include_once("footer.php"); ?>
